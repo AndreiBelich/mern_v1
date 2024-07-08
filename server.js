3be40eb6a7db08a49a825e9ce38b43f8ed63c3db
Belich Andrey <belichandrey89@gmail.com>
2024-07-08 14:40:11 +0300

    Implement errorHandler middleware. Install cors, cookie-parser middlewares. Configured the settings for CORS.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
 const PORT = process.env.PORT || 3500;
 const MODE = process.env.MODE || 'PROD';
 
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(errorHandler);
+
+app.get('/', async(req, res, next) => {
+  res.send({message: "Successuful data"});
+})
 app.listen(PORT, () => {
   if (MODE === 'DEV') {
     console.log(`Server starting on port ${PORT}`);
